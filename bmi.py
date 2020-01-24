a48b99ffa0736137b041b1332a3b58ae7c5ed0ab
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-01-24 01:04:05 +0000

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,9 @@
+# Calculating my Body Mass Index (BMI)
+
+print("Dimitri BMI")
+
+weight = float(input('Enter Dimitri weight: '))
+height = float(input('Enter height: '))
+
+BMI = weight/(height **2)
+print ("Dimitri, your super BMI is", BMI)
